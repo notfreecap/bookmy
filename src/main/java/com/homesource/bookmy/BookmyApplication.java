24bb1800630d18d339c715cf461a2128db9ab55d
notfreecap <andresrenew@gmail.com>
2022-06-04 10:11:31 -0500

    empty project

---
+++ src/main/java/com/homesource/bookmy/BookmyApplication.java
@@ -0,0 +1,13 @@
+package com.homesource.bookmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homesource/bookmy/BookmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homesource.bookmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
